49e54f7dff3b60857844c5c52f4828e3392dbac4
lkfken <lkfken@hotmail.com>
2017-12-12 10:22:07 -0800

    Update one.rb
    show the query when an error is raised

--- lib/sequel/plugins/one.rb
+++ lib/sequel/plugins/one.rb
@@ -8,7 +8,7 @@ module Sequel
       module DatasetMethods
         def one
           rows = fetch_two
-          rows.one? ? rows.first : fail(NotOneRow)
+          rows.one? ? rows.first : fail(NotOneRow, sql)
         end
 
         private
